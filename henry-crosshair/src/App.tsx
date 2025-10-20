d55eeac71d35869703188c1a27dd2b9ea56308e6
Henry <118751951+HenryFtNonna@users.noreply.github.com>
2025-10-20 23:50:20 +0700

    button admin

--- henry-crosshair/src/App.tsx
+++ henry-crosshair/src/App.tsx
@@ -9,7 +9,8 @@ function App() {
   return (
 
       <div>
-        <MainPage/>
+        {/* <MainPage/> */}
+        <AdminDashboard/>
       </div>
 
   )
--- henry-crosshair/src/pages/AdminDashboard.tsx
+++ henry-crosshair/src/pages/AdminDashboard.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from '../components/layout/NavBar'
+import {motion} from 'motion/react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function AdminDashboard() {
   return (
     <div>
       
+      <section className="min-h-screen flex flex-col justify-center items-center bg-white text-gray-900 px-8 py-2">
+        {/* <Navbar/> */}
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="btn btn-circle bg-blue-600 btn-xl border-0 btn-outline">
+          <FontAwesomeIcon icon={faGear} className="text-xl" />
+          
+        </motion.button>
+      </section>
     </div>
   )
 }
