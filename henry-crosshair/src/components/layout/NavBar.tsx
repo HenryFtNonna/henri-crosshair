77d2c533851abb2e253efccea7d5c765c56c005e
Henry <118751951+HenryFtNonna@users.noreply.github.com>
2025-10-21 21:18:05 +0700

    adding navbar, login button, crud button

--- henry-crosshair/src/components/layout/NavBar.tsx
+++ henry-crosshair/src/components/layout/NavBar.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
-import {motion} from 'motion/react';
+import { motion } from 'motion/react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
-    <nav className="w-full fixed top-0 left-0 bg-gray-50 text-white drop-shadow-md z-50 ">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-        {/* Logo */}
-        <h1 className="text-2xl font-bold tracking-wide"></h1>
-
-        {/* Menu */}
-        <ul className="hidden md:flex gap-8 text-lg">
-          <li>
-            <motion.button
-            whileHover={{ scale: 1.1}}
-            whileTap={{ scale: 0.95}}
-            onHoverStart={() => console.log('hover started!')}
-            type="button" 
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-400 dark:focus:ring-gray-200">
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </motion.button>
-          </li>
-        </ul>
-
-      </div>
-    </nav>
+    <div className="fixed navbar bg-gray-100 w-full px-8 py-4 flex justify-end drop-shadow-2xl">
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        transition={{ duration: 0.3 }}
+        className="btn bg-blue-600 text-xl text-white"
+      >
+        Login <FontAwesomeIcon icon={faArrowRight} className="ml-0" /> 
+      </motion.button>
+    </div>
   );
 }
--- henry-crosshair/src/pages/AdminDashboard.tsx
+++ henry-crosshair/src/pages/AdminDashboard.tsx
@@ -2,23 +2,29 @@ import React from 'react'
 import Navbar from '../components/layout/NavBar'
 import {motion} from 'motion/react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function AdminDashboard() {
   return (
     <div>
-      
-      <section className="min-h-screen flex flex-col justify-center items-center bg-white text-gray-900 px-8 py-2">
-        {/* <Navbar/> */}
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          className="btn btn-circle bg-blue-600 btn-xl border-0 btn-outline">
-          <FontAwesomeIcon icon={faGear} className="text-xl" />
-          
-        </motion.button>
-      </section>
+      <Navbar/>
+        <section className="min-h-screen flex flex-col justify-center items-center bg-white text-gray-900">
+
+          {/* Tombol tambah sejajar dengan navbar */}
+          <div className=" fixed bottom-6 w-full flex justify-end px-8">
+            <motion.button
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              className="btn btn-circle btn-lg bg-blue-600 border-0 shadow-lg"
+            >
+              <FontAwesomeIcon icon={faPlus} className="text-[20px] text-white" />
+            </motion.button>
+          </div>
+        </section>
     </div>
   )
 }
