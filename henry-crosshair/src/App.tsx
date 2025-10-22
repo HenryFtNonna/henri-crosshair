e400b30014c19083642152d66ae8d0593a2befcf
Henry <118751951+HenryFtNonna@users.noreply.github.com>
2025-10-22 23:38:13 +0700

    routing

--- henry-crosshair/src/App.tsx
+++ henry-crosshair/src/App.tsx
@@ -1,17 +1,24 @@
 import React from "react"
+import { Route, Routes } from "react-router"
 
 import Login from "./pages/Login"
 import MainPage from "./pages/MainPage" 
 import AdminDashboard from "./pages/AdminDashboard"
+import Navbar from "./components/layout/NavBar"
 
 function App() {
 
   return (
 
       <div>
-        {/* <MainPage/> */}
-        <AdminDashboard/>
-        {/* <Login/> */}
+        <Navbar/>
+        <div>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+        </div>
+
       </div>
 
   )
--- henry-crosshair/src/components/layout/NavBar.tsx
+++ henry-crosshair/src/components/layout/NavBar.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { motion } from 'motion/react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router';
+import Login from '../../pages/Login';
 
 export default function Navbar() {
   return (
     <div className="fixed navbar bg-gray-100 w-full px-8 py-4 flex justify-end drop-shadow-2xl">
       <motion.button
-        whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.95 }}
         transition={{ duration: 0.3 }}
-        className="btn bg-blue-600 text-xl text-white"
+        className="btn bg-blue-600 text-xl text-white rounded-e-lg"
       >
         {/* Login  */}
-        <FontAwesomeIcon icon={faArrowRight} className="ml-0" /> 
+        <Link to={"/Login"}><FontAwesomeIcon icon={faArrowRight}/></Link>
       </motion.button>
     </div>
   );
